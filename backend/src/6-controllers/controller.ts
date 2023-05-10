3297da896efde30c2dd47ad58fc0b8c841c86ee8
Dor Bar <dorbar424@gmail.comgit config --global user.name Dor>
2023-05-10 21:33:24 +0300

    test

---
+++ backend/src/6-controllers/controller.ts
@@ -0,0 +1,42 @@
+import express, {Request, Response, NextFunction} from "express";
+import logic from "../5-logic/logic"
+import MeetingModel from "../4-models/meeting-model";
+
+const router = express.Router()
+
+// GET http://localhost:3001/api/devlopment-groups
+router.get("/devlopment-groups", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const devlopmentGroup = await logic.getAllDevelopmentGroup();
+        response.json(devlopmentGroup);
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+// GET http://localhost:3001/api/meetings-per-development-group/:devlopmentGroupId
+router.get("/meetings-per-development-group/:devlopmentGroupId", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const devlopmentGroupId= +request.params.devlopmentGroupId;
+        const meetings = await logic.getMeetingByGroup(devlopmentGroupId);
+        response.json(meetings);
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+// POST http://localhost:3001/api/meetings
+router.post("/meetings", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const meeting = new MeetingModel(request.body);
+        const addedMeeting = await logic.addMeeting(meeting);
+        response.status(201).json(addedMeeting);
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+export default router;
